28a5b75ea7c9c84ab754ebdcb95951e36ab31afc
minzzun99 <skg06123@naver.com>
2024-08-25 00:54:39 +0900

    Remove: 불필요한 클래스 삭제

--- src/main/java/racingcar/exception/CarNameInputValidator.java
+++
@@ -1,5 +0,0 @@
-package racingcar.exception;
-
-public class CarNameInputValidator {
-    // 자동차 이름을 검증하는 기능 구현
-}
--- src/main/java/racingcar/exception/RaceInputValidator.java
+++
@@ -1,5 +0,0 @@
-package racingcar.exception;
-
-public class RaceInputValidator {
-    // 시도할 횟수를 검증하는 기능 구현
-}
