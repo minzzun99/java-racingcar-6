9122121ce114293304f1f26734c99b447966544d
minzzun99 <skg06123@naver.com>
2024-08-24 23:49:53 +0900

    Comment: InputValidator, InputView 메서드 별 주석 추가

--- src/main/java/racingcar/exception/InputValidator.java
+++ src/main/java/racingcar/exception/InputValidator.java
@@ -3,28 +3,28 @@ package racingcar.exception;
 import static racingcar.exception.message.ErrorMessage.*;
 
 public class InputValidator {
-    // 자동차 이름을 검증하는 기능 구현
-
-    // 자동차 이름을 입력받는 줄에 쉼표가 있는지 검증
     public static void carNameInputLineValid(String line) {
+        // 자동차 이름을 입력받는 줄에 쉼표가 있는지 검증
         if (!line.contains(",")) {
             throw new IllegalArgumentException(String.valueOf(NOT_CONTAINS_COMMA_ERROR));
         }
     }
 
-    // 자동차 이름이 유효한지 검증
     public static void carNameInputValid(String name) {
+        // 자동차 이름이 5자 이하인지 확인
         if (name.length() > 5) {
             throw new IllegalArgumentException(String.valueOf(CAR_NAME_LENGTH_ERROR));
         }
     }
 
     public static void inputNumberOfRaceValid(String str) {
+        // 입력받은 문자열이 숫자가 맞는지
         boolean isNumeric = str.matches("[+-]?\\d*(\\.\\d+)?");
         if (!isNumeric) {
             throw new IllegalArgumentException(String.valueOf(NOT_NUMBER_ERROR));
         }
 
+        // 입력받은 숫자가 1 이상인지 확인
         if (Integer.parseInt(str) <= 0) {
             throw new IllegalArgumentException(String.valueOf(NEGATIVE_NUMBER_ERROR));
         }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,19 @@
 package racingcar.view;
 
-
 import static racingcar.view.OutputView.*;
 import static racingcar.view.message.RaceMessage.*;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    // 입력받는 부분을 담당
+    // 자동차의 이름을 입력받는 메서드
     public static String inputCarName() {
         printRaceMessage(CAR_NAME_INPUT);
-        return Console.readLine();
+        return Console.readLine().replaceAll("\\s", "");        // 공백제거
     }
 
+
+    // 레이스의 시도 횟수를 입력받는 메서드
     public static String inputNumberOfRace() {
         printRaceMessage(NUMBER_OF_RACE_INPUT);
         return Console.readLine();
