1ec14b8a9ab5ce85d8487c50f5bf916dd4cc23b5
minzzun99 <skg06123@naver.com>
2024-08-25 00:55:44 +0900

    Feat: 자동차를 움직이는 기능 추가

--- src/main/java/racingcar/service/CarMove.java
+++ src/main/java/racingcar/service/CarMove.java
@@ -1,5 +1,26 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import racingcar.domain.Car;
+
 public class CarMove {
-    // 자동차를 이동하는 클래스
+    private static int START_RANGE = 0;
+    private static int END_RANGE = 9;
+    private static int MOVE_THRESHOLD = 4;
+
+    public static void moveCar(List<Car> cars) {
+        // 각 자동차를 이동하는 메서드
+        for (int i = 0; i < cars.size(); i++) {
+            int randomNumber = generateRandomNumber();
+            if (randomNumber >= MOVE_THRESHOLD) {
+                cars.get(i).moveLocation(true);
+            }
+        }
+
+    }
+
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(START_RANGE, END_RANGE);
+    }
 }
