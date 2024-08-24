57eb030ffb132e626b86209788908cce47a5e4b1
minzzun99 <skg06123@naver.com>
2024-08-24 23:20:30 +0900

    Feat: Car 클래스 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,4 +2,13 @@ package racingcar.domain;
 
 public class Car {
     // 자동차의 이름, 랜덤 숫자
+    private final String carName;
+    private int randomNumber;
+    private int location;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.randomNumber = 0;
+        this.location = 0;
+    }
 }
