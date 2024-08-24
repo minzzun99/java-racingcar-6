68bbf9816ecb6ae8d3607e6b84f72ae66b4ae9fe
minzzun99 <skg06123@naver.com>
2024-08-24 23:45:34 +0900

    Feat: 자동차의 이름을 쉼표로 나누는 기능 구현

---
+++ src/main/java/racingcar/service/CarNameTokenizer.java
@@ -0,0 +1,27 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import racingcar.domain.Car;
+import racingcar.exception.InputValidator;
+
+public class CarNameTokenizer {
+    public static List<Car> setCarList(String input) {
+        // 자동차의 이름을 쉼표로 나누고 리스트에 저장해서 return하는 메서드
+        List<Car> carList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(input, ",");
+
+        while (st.hasMoreTokens()) {
+            try {
+                String carName = st.nextToken();
+                InputValidator.carNameInputValid(carName);
+                carList.add(new Car(carName));
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException(e.getMessage());
+            }
+        }
+
+        return carList;
+    }
+}
