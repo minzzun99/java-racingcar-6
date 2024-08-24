27cb41ae61872bac2a4147f87c6bbeab6198616a
minzzun99 <skg06123@naver.com>
2024-08-25 00:56:50 +0900

    Feat: Car의 이름과 위치를 반환하는 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,26 @@
 package racingcar.domain;
 
 public class Car {
-    // 자동차의 이름, 랜덤 숫자
+    // 자동차의 이름, 현재 위치
     private final String carName;
-    private int randomNumber;
     private int location;
 
     public Car(String carName) {
         this.carName = carName;
-        this.randomNumber = 0;
         this.location = 0;
+    }
+
+    public void moveLocation(boolean move) {
+        if (move) {
+            this.location++;
+        }
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getLocation() {
+        return location;
     }
 }
